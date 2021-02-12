64cd7b6f341bb2a36fece04c4f3d9802a2fabe49
Scott J Miles <sjmiles@google.com>
2021-02-11 17:42:32 -0800

    fix blunders

--- ts/test.ts
+++ ts/test.ts
@@ -1,7 +1,7 @@
 import {A} from './a_needs_b.js';
 import {B} from './b_needs_a.js';
 
-const b: B = new A().makeB();
-const a: A = new B().makeA();
+const b: B = new A().makeB() as B;
+const a: A = new B().makeA() as A;
 
 console.log(a, b);
